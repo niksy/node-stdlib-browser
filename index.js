321805223cd8c2ac9ddc2ff7dcd5ed6c3bb0adf5
Ivan Nikolić <niksy5@gmail.com>
2021-06-30 19:18:29 +0200

    Add changes for main export

--- index.js
+++ index.js
@@ -1,38 +1,83 @@
-exports.assert = require.resolve('assert/');
-exports.buffer = require.resolve('buffer/');
-exports.child_process = null;
-exports.cluster = null;
-exports.console = require.resolve('console-browserify');
-exports.constants = require.resolve('constants-browserify');
-exports.crypto = require.resolve('crypto-browserify');
-exports.dgram = null;
-exports.dns = null;
-exports.domain = require.resolve('domain-browser');
-exports.events = require.resolve('events/');
-exports.fs = null;
-exports.http = require.resolve('stream-http');
-exports.https = require.resolve('https-browserify');
-exports.module = null;
-exports.net = null;
-exports.os = require.resolve('os-browserify/browser.js');
-exports.path = require.resolve('path-browserify');
-exports.punycode = require.resolve('punycode/');
-exports.process = require.resolve('process/browser.js');
-exports.querystring = require.resolve('querystring-es3/');
-exports.readline = null;
-exports.repl = null;
-exports.stream = require.resolve('stream-browserify');
-exports._stream_duplex = require.resolve('readable-stream/duplex.js');
-exports._stream_passthrough = require.resolve('readable-stream/passthrough.js');
-exports._stream_readable = require.resolve('readable-stream/readable.js');
-exports._stream_transform = require.resolve('readable-stream/transform.js');
-exports._stream_writable = require.resolve('readable-stream/writable.js');
-exports.string_decoder = require.resolve('string_decoder/');
-exports.sys = require.resolve('util/util.js');
-exports.timers = require.resolve('timers-browserify');
-exports.tls = null;
-exports.tty = require.resolve('tty-browserify');
-exports.url = require.resolve('url/');
-exports.util = require.resolve('util/util.js');
-exports.vm = require.resolve('vm-browserify');
-exports.zlib = require.resolve('browserify-zlib');
+import { createRequire } from 'module';
+
+const require = globalThis.require ?? createRequire(import.meta.url);
+
+const assert = require.resolve('assert/');
+const buffer = require.resolve('buffer/');
+const child_process = null;
+const cluster = null;
+const _console = require.resolve('console-browserify');
+const constants = require.resolve('constants-browserify');
+const crypto = require.resolve('crypto-browserify');
+const dgram = null;
+const dns = null;
+const domain = require.resolve('domain-browser');
+const events = require.resolve('events/');
+const fs = null;
+const http = require.resolve('stream-http');
+const https = require.resolve('https-browserify');
+const _module = null;
+const net = null;
+const os = require.resolve('os-browserify/browser.js');
+const path = require.resolve('path-browserify');
+const punycode = require.resolve('punycode/');
+const _process = require.resolve('process/browser.js');
+const querystring = require.resolve('querystring-es3/');
+const readline = null;
+const repl = null;
+const stream = require.resolve('stream-browserify');
+const _stream_duplex = require.resolve('readable-stream/duplex.js');
+const _stream_passthrough = require.resolve('readable-stream/passthrough.js');
+const _stream_readable = require.resolve('readable-stream/readable.js');
+const _stream_transform = require.resolve('readable-stream/transform.js');
+const _stream_writable = require.resolve('readable-stream/writable.js');
+const string_decoder = require.resolve('string_decoder/');
+const sys = require.resolve('util/util.js');
+const timers = require.resolve('timers-browserify');
+const tls = null;
+const tty = require.resolve('tty-browserify');
+const url = require.resolve('url/');
+const util = require.resolve('util/util.js');
+const vm = require.resolve('vm-browserify');
+const zlib = require.resolve('browserify-zlib');
+
+export {
+	assert,
+	buffer,
+	child_process,
+	cluster,
+	_console as console,
+	constants,
+	crypto,
+	dgram,
+	dns,
+	domain,
+	events,
+	fs,
+	http,
+	https,
+	_module as module,
+	net,
+	os,
+	path,
+	punycode,
+	_process as process,
+	querystring,
+	readline,
+	repl,
+	stream,
+	_stream_duplex,
+	_stream_passthrough,
+	_stream_readable,
+	_stream_transform,
+	_stream_writable,
+	string_decoder,
+	sys,
+	timers,
+	tls,
+	tty,
+	url,
+	util,
+	vm,
+	zlib
+};
