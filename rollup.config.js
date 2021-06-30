b35e30d841942d304a6c22275019a3484ed8eb77
Ivan Nikolić <niksy5@gmail.com>
2021-06-30 20:23:19 +0200

    Adjust Rollup configuration for mocks

--- rollup.config.js
+++ rollup.config.js
@@ -4,51 +4,69 @@ const path = require('path');
 const { promises: fs } = require('fs');
 const { default: babel } = require('@rollup/plugin-babel');
 
-module.exports = {
-	input: 'index.js',
-	output: [
-		{
-			file: 'cjs/index.js',
-			format: 'cjs',
-			exports: 'auto',
-			sourcemap: true
-		},
-		{
-			file: 'esm/index.js',
-			format: 'esm',
-			sourcemap: true
-		}
-	],
-	plugins: [
-		(() => {
-			return {
-				name: 'package-type',
-				async writeBundle(output) {
-					let prefix, type;
-					if (output.file.includes('cjs/')) {
-						prefix = 'cjs';
-						type = 'commonjs';
-					} else if (output.file.includes('esm/')) {
-						prefix = 'esm';
-						type = 'module';
+function getConfig(filename) {
+	return {
+		input: filename,
+		output: [
+			{
+				file: `cjs/${filename}`,
+				format: 'cjs',
+				exports: 'auto',
+				sourcemap: true
+			},
+			{
+				file: `esm/${filename}`,
+				format: 'esm',
+				sourcemap: true
+			}
+		],
+		plugins: [
+			(() => {
+				return {
+					name: 'package-type',
+					async writeBundle(output) {
+						let prefix, type;
+						if (output.file.includes('index.js')) {
+							return;
+						}
+						if (output.file.includes('cjs/')) {
+							prefix = 'cjs';
+							type = 'commonjs';
+						} else if (output.file.includes('esm/')) {
+							prefix = 'esm';
+							type = 'module';
+						}
+						if (typeof prefix !== 'undefined') {
+							const package_ = path.join(prefix, 'package.json');
+							try {
+								await fs.unlink(package_);
+							} catch (error) {}
+							await fs.writeFile(
+								package_,
+								JSON.stringify({ type }),
+								'utf8'
+							);
+						}
 					}
-					if (typeof prefix !== 'undefined') {
-						const package_ = path.join(prefix, 'package.json');
-						try {
-							await fs.unlink(package_);
-						} catch (error) {}
-						await fs.writeFile(
-							package_,
-							JSON.stringify({ type }),
-							'utf8'
-						);
-					}
-				}
-			};
-		})(),
-		babel({
-			babelHelpers: 'bundled',
-			exclude: 'node_modules/**'
-		})
-	]
-};
+				};
+			})(),
+			babel({
+				babelHelpers: 'bundled',
+				exclude: 'node_modules/**'
+			})
+		]
+	};
+}
+
+module.exports = [
+	'index.js',
+	'mock/buffer.js',
+	'mock/console.js',
+	'mock/dns.js',
+	'mock/empty.js',
+	'mock/net.js',
+	'mock/process.js',
+	'mock/punycode.js',
+	'mock/tls.js',
+	'mock/tty.js'
+].map((filename) => getConfig(filename));
