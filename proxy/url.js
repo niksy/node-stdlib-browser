8afe7fc987f191447e12fd1a3c4591f00a2ad440
Ivan Nikolić <niksy5@gmail.com>
2021-10-04 09:52:50 +0200

    Add types for `url` methods

--- proxy/url.js
+++ proxy/url.js
@@ -1,7 +1,25 @@
 /* globals unknown */
 
+/**
+ * @typedef {import('url').URLFormatOptions} URLFormatOptions
+ * @typedef {import('url').UrlObject} UrlObject
+ * @typedef {import('url').format} formatImport
+ * @typedef {import('url').parse} parseImport
+ * @typedef {import('url').resolve} resolveImport
+ * @typedef {import('url').Url} UrlImport
+ * @typedef {import('url').fileURLToPath} fileURLToPath
+ * @typedef {import('url').pathToFileURL} pathToFileURL
+ * @typedef {import('url').domainToUnicode} domainToUnicode
+ * @typedef {import('url').domainToASCII} domainToASCII
+ */
+
 // @ts-ignore
 import { format, parse, resolve, resolveObject, Url } from 'native-url';
+
+const formatImport = /** @type {formatImport}*/ (format);
+const parseImport = /** @type {parseImport}*/ (parse);
+const resolveImport = /** @type {resolveImport}*/ (resolve);
+const UrlImport = /** @type {UrlImport}*/ (Url);
 
 const URL = globalThis.URL;
 /* eslint-disable-next-line unicorn/prevent-abbreviations */
@@ -68,67 +86,105 @@ function encodePathChars(filepath) {
 	return filepath;
 }
 
-/**
- * @param {unknown[]} arguments_
- */
-function domainToASCII(...arguments_) {
-	if (arguments_.length === 0) {
-		throw new TypeError('The "domain" argument must be specified');
-	}
-	const [domain] = arguments_;
-	return new URL(`http://${domain}`).hostname;
-}
+const domainToASCII =
+	/**
+	 * @type {domainToASCII}
+	 */
+	function (domain) {
+		if (typeof domain === 'undefined') {
+			throw new TypeError('The "domain" argument must be specified');
+		}
+		return new URL(`http://${domain}`).hostname;
+	};
 
-/**
- * @param {unknown[]} arguments_
- */
-function domainToUnicode(...arguments_) {
-	if (arguments_.length === 0) {
-		throw new TypeError('The "domain" argument must be specified');
-	}
-	const [domain] = arguments_;
-	return new URL(`http://${domain}`).hostname;
-}
+const domainToUnicode =
+	/**
+	 * @type {domainToUnicode}
+	 */
+	function (domain) {
+		if (typeof domain === 'undefined') {
+			throw new TypeError('The "domain" argument must be specified');
+		}
+		return new URL(`http://${domain}`).hostname;
+	};
 
-/**
- * @param {string} filepath
- */
-function pathToFileURL(filepath) {
-	const outURL = new URL('file://');
-	let resolved = filepath;
-	const filePathLast = filepath.charCodeAt(filepath.length - 1);
-	if (
-		filePathLast === CHAR_FORWARD_SLASH &&
-		resolved[resolved.length - 1] !== '/'
-	) {
-		resolved += '/';
-	}
-	outURL.pathname = encodePathChars(resolved);
-	return outURL;
-}
+const pathToFileURL =
+	/**
+	 * @type {(url: string) => URL}
+	 */
+	function (filepath) {
+		const outURL = new URL('file://');
+		let resolved = filepath;
+		const filePathLast = filepath.charCodeAt(filepath.length - 1);
+		if (
+			filePathLast === CHAR_FORWARD_SLASH &&
+			resolved[resolved.length - 1] !== '/'
+		) {
+			resolved += '/';
+		}
+		outURL.pathname = encodePathChars(resolved);
+		return outURL;
+	};
 
-/**
- * @param {URL|string} path
- */
-function fileURLToPath(path) {
-	if (!isURLInstance(path) && typeof path !== 'string') {
-		throw new TypeError(
-			`The "path" argument must be of type string or an instance of URL. Received type ${typeof path} (${path})`
-		);
-	}
-	const resolved = new URL(path);
-	if (resolved.protocol !== 'file:') {
-		throw new TypeError('The URL must be of scheme file');
-	}
-	return getPathFromURLPosix(resolved);
-}
+const fileURLToPath =
+	/**
+	 * @type {fileURLToPath}
+	 */
+	function (path) {
+		if (!isURLInstance(path) && typeof path !== 'string') {
+			throw new TypeError(
+				`The "path" argument must be of type string or an instance of URL. Received type ${typeof path} (${path})`
+			);
+		}
+		const resolved = new URL(path);
+		if (resolved.protocol !== 'file:') {
+			throw new TypeError('The URL must be of scheme file');
+		}
+		return getPathFromURLPosix(resolved);
+	};
+
+const formatImportWithOverloads =
+	/**
+	 * @type {(
+	 *   ((urlObject: URL, options?: URLFormatOptions) => string) &
+	 *   ((urlObject: UrlObject | string, options?: never) => string)
+	 * )}
+	 */
+	function (urlObject, options) {
+		if (!(urlObject instanceof URL)) {
+			return formatImport(urlObject);
+		}
+		const { auth, fragment, search, unicode } = options ?? {
+			auth: true,
+			fragment: true,
+			search: true,
+			unicode: false
+		};
+
+		const parsed = parseImport(urlObject.toString());
+
+		if (!auth) {
+			parsed.auth = null;
+		}
+
+		if (!fragment) {
+			parsed.hash = null;
+		}
+
+		if (!search) {
+			parsed.search = null;
+			parsed.query = null;
+		}
+
+		return formatImport(parsed);
+	};
 
 const api = {
-	format,
-	parse,
-	resolve,
+	format: formatImportWithOverloads,
+	parse: parseImport,
+	resolve: resolveImport,
 	resolveObject,
-	Url,
+	Url: UrlImport,
 	URL,
 	URLSearchParams,
 	domainToASCII,
@@ -140,11 +196,11 @@ const api = {
 export default api;
 
 export {
-	format,
-	parse,
-	resolve,
+	formatImportWithOverloads as format,
+	parseImport as parse,
+	resolveImport as resolve,
 	resolveObject,
-	Url,
+	UrlImport as Url,
 	URL,
 	URLSearchParams,
 	domainToASCII,
