57c926ae0fb52354b1796cf362ea13ab6b7a6af3
Ivan Nikolić <niksy5@gmail.com>
2021-12-08 09:22:21 +0100

    Simplify Webpack plugin
    Ref. https://github.com/webpack/webpack/issues/13290#issuecomment-987880453

--- helpers/webpack/plugin.js
+++ helpers/webpack/plugin.js
@@ -1,48 +1,16 @@
 'use strict';
 
-const { URL, fileURLToPath } = require('url');
+const webpack = require('webpack');
 
-class NodeProtocolUrlPlugin {
-	apply(compiler) {
-		compiler.hooks.compilation.tap(
-			'NodeProtocolUrlPlugin',
-			(compilation, { normalModuleFactory }) => {
-				const resolver = compilation.resolverFactory.get('normal');
-				const context = compilation.compiler.context;
+const nodeProtocolRegex = /^node:/;
 
-				normalModuleFactory.hooks.resolveForScheme
-					.for('node')
-					.tapPromise('NodeProtocolUrlPlugin', (resourceData) => {
-						return new Promise((resolve, reject) => {
-							resolver.resolve(
-								{},
-								context,
-								resourceData.resource,
-								{},
-								(error, resolvedModuleId) => {
-									if (error) {
-										reject(error);
-										return;
-									}
-									const url = new URL(
-										`file://${resolvedModuleId}`
-									);
-									const path = fileURLToPath(url);
-									const query = url.search;
-									const fragment = url.hash;
-									resourceData.path = path;
-									resourceData.query = query;
-									resourceData.fragment = fragment;
-									resourceData.resource =
-										path + query + fragment;
-									resolve(true);
-								}
-							);
-						});
-					});
-			}
-		);
-	}
+function NodeProtocolUrlPlugin() {
+	return new webpack.NormalModuleReplacementPlugin(
+		nodeProtocolRegex,
+		(resource) => {
+			resource.request = resource.request.replace(nodeProtocolRegex, '');
+		}
+	);
 }
 
 module.exports.NodeProtocolUrlPlugin = NodeProtocolUrlPlugin;
